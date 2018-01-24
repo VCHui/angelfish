6db17bae7c0819ec20aaa32a0868313b44f4c5b2
Victor Hui <23016403+vc-h@users.noreply.github.com>
2018-01-24 07:24:19 +0000

    :func:`vrepr` to return piece positions only:
    	modified:   posvector.py

--- posvector.py
+++ posvector.py
@@ -10,103 +10,81 @@ piecetypes = 'PNBRQKpnbrqk'
 
 def vrepr(pos):
     """
-    encode pos into an array of ``12*64 = 768`` elements
+    encode pos.board into an array of 12 bit boards for piecetypes:
 
-      - square has a value 2 if occupied
-      - square has a value 1 if possibly occupied
-      - king square is 3 or 5 for possible left or right castling
+    >>> assert len(piecetypes) == 12
+    >>> assert piecetypes[:6] == piecetypes[6:].upper()
 
-    * Game opening
+    * An example of the game opening
 
-    >>> p0 = tools.parseFEN(tools.FEN_INITIAL)
-    >>> v0 = vrepr(p0)
+      >>> p0 = tools.parseFEN(tools.FEN_INITIAL)
+      >>> "".join(p0.board.split()) == (
+      ... 'rnbqkbnr'
+      ... 'pppppppp'
+      ... '........'
+      ... '........'
+      ... '........'
+      ... '........'
+      ... 'PPPPPPPP'
+      ... 'RNBQKBNR' )
+      True
 
-    * To input to a neural network (without normalization)
-    >>> v = np.array(v0).flatten().astype(float)
-    >>> len(v)
-    768
+      >>> v0 = vrepr(p0)
+      >>> assert len(v0) == 12
+      >>> assert all(map((64).__eq__,(map(len,v0))))
+      >>> assert all(map(np.dtype(bool).__eq__,map(np.result_type,v0)))
 
-    >>> v0[piecetypes.find("P")].reshape(8,8)
-    array([[0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [1, 1, 1, 1, 1, 1, 1, 1],
-           [1, 1, 1, 1, 1, 1, 1, 1],
-           [2, 2, 2, 2, 2, 2, 2, 2],
-           [0, 0, 0, 0, 0, 0, 0, 0]])
-    >>> v0[piecetypes.find("p")].reshape(8,8)
-    array([[0, 0, 0, 0, 0, 0, 0, 0],
-           [2, 2, 2, 2, 2, 2, 2, 2],
-           [1, 1, 1, 1, 1, 1, 1, 1],
-           [1, 1, 1, 1, 1, 1, 1, 1],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0]])
+      >>> assert all(np.packbits(v0) == np.array([
+      ...   0,   0,   0,   0,   0,   0, 255,   0, # P
+      ...   0,   0,   0,   0,   0,   0,   0,  66, # N
+      ...   0,   0,   0,   0,   0,   0,   0,  36, # B
+      ...   0,   0,   0,   0,   0,   0,   0, 129, # R
+      ...   0,   0,   0,   0,   0,   0,   0,  16, # Q
+      ...   0,   0,   0,   0,   0,   0,   0,   8, # K
+      ...   0, 255,   0,   0,   0,   0,   0,   0, # p
+      ...  66,   0,   0,   0,   0,   0,   0,   0, # n
+      ...  36,   0,   0,   0,   0,   0,   0,   0, # b
+      ... 129,   0,   0,   0,   0,   0,   0,   0, # r
+      ...  16,   0,   0,   0,   0,   0,   0,   0, # q
+      ...   8,   0,   0,   0,   0,   0,   0,   0, # k
+      ... ]))
 
-    * Black's turn, white checkmate!
+    * An example white checkmate!
 
-    >>> p1 = tools.parseFEN('7k/6Q1/5K2/8/8/8/8/8 b - - 0 1')
-    >>> "".join(p1.rotate().board.split()) == (
-    ... '.......k'
-    ... '......Q.'
-    ... '.....K..'
-    ... '........'
-    ... '........'
-    ... '........'
-    ... '........'
-    ... '........' )
-    True
-    >>> v1 = vrepr(p1)
-    >>> v1[piecetypes.find("k")].reshape(8,8)
-    array([[0, 0, 0, 0, 0, 0, 0, 2],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0]])
-    >>> v1[piecetypes.find("K")].reshape(8,8)
-    array([[0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 1, 1, 0, 0],
-           [0, 0, 0, 0, 1, 2, 1, 0],
-           [0, 0, 0, 0, 1, 1, 1, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0]])
-    >>> v1[piecetypes.find("Q")].reshape(8,8)
-    array([[0, 0, 0, 0, 0, 1, 1, 1],
-           [1, 1, 1, 1, 1, 1, 2, 1],
-           [0, 0, 0, 0, 0, 0, 1, 1],
-           [0, 0, 0, 0, 0, 0, 1, 0],
-           [0, 0, 0, 0, 0, 0, 1, 0],
-           [0, 0, 0, 0, 0, 0, 1, 0],
-           [0, 0, 0, 0, 0, 0, 1, 0],
-           [0, 0, 0, 0, 0, 0, 1, 0]])
+      >>> p1 = tools.parseFEN('7k/6Q1/5K2/8/8/8/8/8 b - - 0 1')
+      >>> "".join(p1.rotate().board.split()) == ( # un-rotating for black
+      ... '.......k'
+      ... '......Q.'
+      ... '.....K..'
+      ... '........'
+      ... '........'
+      ... '........'
+      ... '........'
+      ... '........' )
+      True
+
+      >>> v1 = vrepr(p1)
+      >>> assert all(np.packbits(v1) == np.array([
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # P
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # N
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # B
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # R
+      ... 0, 2, 0, 0, 0, 0, 0, 0, # Q
+      ... 0, 0, 4, 0, 0, 0, 0, 0, # K
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # p
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # n
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # b
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # r
+      ... 0, 0, 0, 0, 0, 0, 0, 0, # q
+      ... 1, 0, 0, 0, 0, 0, 0, 0, # k
+      ... ]))
 
     """
 
     if pos.board.startswith("\n"): # is black
         pos = pos.rotate() # un-rotate to the  conventional orientation
     squares = np.array(tuple("".join(pos.board.split()))) # strip
-    v = [(squares == piece)*2 for piece in piecetypes]
-    v[piecetypes.find("K")] = (squares == "K")*(2**pos.wc[0] + 4**pos.wc[1])
-    v[piecetypes.find("k")] = (squares == "k")*(2**pos.bc[0] + 4**pos.bc[1])
-    moves = tuple(move for move in pos.gen_moves() if legal(pos,move))
-    for i,j in moves:
-        ipiece = piecetypes.find(pos.board[i])
-        jsq = ((j//10) - 2)*8 + ((j%10) - 1)
-        v[ipiece][jsq] = 1
-    pos = pos.rotate()
-    moves = tuple(move for move in pos.gen_moves() if legal(pos,move))
-    for i,j in moves:
-        ipiece = 6 + piecetypes.find(pos.board[i]) # lower case piece
-        jsq = ((j//10) - 2)*8 + ((j%10) - 1) + 1
-        v[ipiece][-jsq] = 1 # un-rotating jsq
-    return v
+    return [(squares == piece) for piece in piecetypes]
 
 if __name__ == '__main__':
 
